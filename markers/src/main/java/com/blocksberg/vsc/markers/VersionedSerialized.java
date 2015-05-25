7461d56d1b84a249a6c9784fa2f0a08096fcf031
Justin Heesemann <jheesemann@gmail.com>
2015-05-25 13:26:57 +0200

    initial version

---
+++ markers/src/main/java/com/blocksberg/vsc/markers/VersionedSerialized.java
@@ -0,0 +1,23 @@
+package com.blocksberg.vsc.markers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author jh
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface VersionedSerialized {
+    String id();
+
+    int version() default 0;
+
+    SerializationTechnique serialization() default SerializationTechnique.JAVA;
+
+    enum SerializationTechnique {
+        JAVA, JACKSON, GSON;
+    }
+}
